51e21bcfd6df195f3f5431779649f422acf40c85
Rex Ledesma <rex@rexledesma.com>
2025-04-05 18:44:55 -0400

    feat: login to foodcoop using `playwright`

--- main.py
+++ main.py
@@ -1,5 +1,37 @@
+import os
+
+from playwright.sync_api import sync_playwright
+
+FOODCOOP_USERNAME = os.getenv("FOODCOOP_USERNAME")
+FOODCOOP_PASSWORD = os.getenv("FOODCOOP_PASSWORD")
+
+FOODCOOP_USERNAME_INPUT = "Member Number or Email"
+FOODCOOP_PASSWORD_INPUT = "Password"
+FOODCOOP_LOGIN_BUTTON = "Log In"
+
+FOODCOOP_LOGIN_URL = "https://members.foodcoop.com/services/login/"
+
+
 def main():
-    print("Hello from foodcoop-shift-calendar!")
+    assert FOODCOOP_USERNAME, "FOODCOOP_USERNAME is not set"
+    assert FOODCOOP_PASSWORD, "FOODCOOP_PASSWORD is not set"
+
+    with sync_playwright() as p:
+        browser = p.chromium.launch()
+        page = browser.new_page()
+
+        # Login to the coop
+        page.goto(FOODCOOP_LOGIN_URL)
+
+        page.get_by_role("textbox", name=FOODCOOP_USERNAME_INPUT).fill(
+            FOODCOOP_USERNAME
+        )
+        page.get_by_role("textbox", name=FOODCOOP_PASSWORD_INPUT).fill(
+            FOODCOOP_PASSWORD
+        )
+        page.get_by_role("button", name=FOODCOOP_LOGIN_BUTTON).click()
+
+        browser.close()
 
 
 if __name__ == "__main__":
